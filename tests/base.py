be39b7d10e5e4136e549d9ae9cc437e83433bf05
henry199101 <331243543@qq.com>
2019-01-20 16:57:31 +0800

    删除两条用例，没有太大的必要进行测试。

--- tests/base.py
+++ tests/base.py
@@ -13,9 +13,3 @@ class BasicTestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
-
-    def test_app_exists(self):
-        self.assertIsNotNone(current_app)
-
-    def test_app_is_testing(self):
-        self.assertTrue(current_app.config['TESTING'])
