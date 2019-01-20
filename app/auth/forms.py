4d1196a6d3c59fce4744870f02ec152908310f2a
henry199101 <331243543@qq.com>
2019-01-20 23:38:31 +0800

    增加用户注册的表单，有电子邮件、用户名、密码的【客户端 & 服务器端验证】。

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +10,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    username = StringField('Username')
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm password', validators=[
+        DataRequired(), EqualTo('password', message='Password must match.')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first() is not None:
+            raise ValidationError('The email has been registered !')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first() is not None:
+            raise ValidationError('The username has been registered !')
