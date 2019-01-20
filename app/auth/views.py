7e6c265ef29125b2fe6e6d09c9d2df8358877d23
henry199101 <331243543@qq.com>
2019-01-20 16:37:44 +0800

    修改login视图，增加功能【服务器端邮箱、用户名、密码校验】、【如果不是相对URL，就重定向到首页】。

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,30 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from . import auth
+from flask_login import login_required
+from .forms import LoginForm
+from app.models import User
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+
+        password = form.password.data
+
+        if user is not None and user.verify_password(password):
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid email or password.')
+
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/sec')
+@login_required
+def sec():
+    return '只有登录用户才能访问这个页面！'
